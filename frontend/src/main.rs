218833b8f0e79f5f1836dc44be141febcb2ebf42
Elia Bieri <contact@eliabieri.com>
2022-12-12 19:01:14 +0100

    Fix dashboard on small screens

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -86,64 +86,67 @@ fn main_component() -> Html {
         })
     };
     html! {
-        <div class="flex items-center justify-center h-screen bg-zinc-700">
-            // Card
-            <div class="bg-zinc-800 rounded-2xl p-5 shadow-2xl">
-                // Flex Container
-                <div class="flex flex-col">
-                    // Image
-                    <img src="assets/logo.png" alt="" class="h-24 hover:scale-110 duration-500 object-contain"/>
-                    // Content
-                    <div class="p-3">
-                        <p class="text-white leading-5 my-4 text-2xl font-bold tracking-wide text-center md:text-left">
-                            {"Configuration"}
-                        </p>
+        <div>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <div class="flex items-center justify-center h-screen w-screen bg-zinc-700">
+                // Card
+                <div class="bg-zinc-800 rounded-2xl p-5 shadow-2xl">
+                    // Flex Container
+                    <div class="flex flex-col">
+                        // Image
+                        <img src="assets/logo.png" alt="" class="h-24 hover:scale-110 duration-500 object-contain"/>
+                        // Content
+                        <div class="p-3">
+                            <p class="text-white leading-5 my-4 text-2xl font-bold tracking-wide text-center">
+                                {"Configuration"}
+                            </p>
 
-                        <div>
-                            <ConfigCardComponent>
-                                <DefaultWidgetConfigComponent
-                                    widget_name={WidgetName::Time.as_str()}
-                                    config={system_config.widget_config.time_config.clone()}
-                                on_change={update_time_config}
-                                />
-                            </ConfigCardComponent>
-
-                            <ConfigCardComponent>
-                                <DefaultWidgetConfigComponent
-                                    widget_name={WidgetName::Aare.as_str()}
-                                    config={system_config.widget_config.aare_config.clone()}
-                                on_change={update_aare_config}
-                                />
-                            </ConfigCardComponent>
-
-                            <ConfigCardComponent>
-                                <DefaultWidgetConfigComponent
-                                    widget_name={WidgetName::Cafete.as_str()}
-                                    config={system_config.widget_config.cafete_config.clone()}
-                                on_change={update_cafete_config}
-                                />
-                            </ConfigCardComponent>
-
-                            <ConfigCardComponent>
-                                <DefaultWidgetConfigComponent
-                                    widget_name={WidgetName::Bernaqua.as_str()}
-                                    config={system_config.widget_config.bernaqua_config.clone()}
-                                on_change={update_bernaqua_config}
-                                />
-                            </ConfigCardComponent>
-
-                            <ConfigCardComponent>
-                                <WidgetConfigComponent
-                                    widget_name={WidgetName::PublicTransport.as_str()}
-                                    config={system_config.widget_config.public_transport_config.base_config.clone()}
-                                    on_change={update_public_transport_base_config}
-                                >
-                                    <PublicTransportConfigComponent
-                                        config={system_config.widget_config.public_transport_config.clone()}
-                                        on_change={update_public_transport_config}
+                            <div>
+                                <ConfigCardComponent>
+                                    <DefaultWidgetConfigComponent
+                                        widget_name={WidgetName::Time.as_str()}
+                                        config={system_config.widget_config.time_config.clone()}
+                                    on_change={update_time_config}
                                     />
-                                </WidgetConfigComponent>
-                            </ConfigCardComponent>
+                                </ConfigCardComponent>
+
+                                <ConfigCardComponent>
+                                    <DefaultWidgetConfigComponent
+                                        widget_name={WidgetName::Aare.as_str()}
+                                        config={system_config.widget_config.aare_config.clone()}
+                                    on_change={update_aare_config}
+                                    />
+                                </ConfigCardComponent>
+
+                                <ConfigCardComponent>
+                                    <DefaultWidgetConfigComponent
+                                        widget_name={WidgetName::Cafete.as_str()}
+                                        config={system_config.widget_config.cafete_config.clone()}
+                                    on_change={update_cafete_config}
+                                    />
+                                </ConfigCardComponent>
+
+                                <ConfigCardComponent>
+                                    <DefaultWidgetConfigComponent
+                                        widget_name={WidgetName::Bernaqua.as_str()}
+                                        config={system_config.widget_config.bernaqua_config.clone()}
+                                    on_change={update_bernaqua_config}
+                                    />
+                                </ConfigCardComponent>
+
+                                <ConfigCardComponent>
+                                    <WidgetConfigComponent
+                                        widget_name={WidgetName::PublicTransport.as_str()}
+                                        config={system_config.widget_config.public_transport_config.base_config.clone()}
+                                        on_change={update_public_transport_base_config}
+                                    >
+                                        <PublicTransportConfigComponent
+                                            config={system_config.widget_config.public_transport_config.clone()}
+                                            on_change={update_public_transport_config}
+                                        />
+                                    </WidgetConfigComponent>
+                                </ConfigCardComponent>
+                            </div>
                         </div>
                     </div>
                 </div>
