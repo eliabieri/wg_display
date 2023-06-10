67090bf81aa420db1b709ef0d161d22047eaee6d
Elia Bieri <contact@eliabieri.com>
2023-06-10 12:18:18 +0200

    Update dependencies

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -75,7 +75,7 @@ pub fn home() -> Html {
     };
 
     html! {
-        <div class="bg-zinc-400 h-full">
+        <div class="h-full">
             <meta name="viewport" content="width=device-width initial-scale=1.0"/>
             <div class="flex flex-col items-center justify-center">
                 // Card
