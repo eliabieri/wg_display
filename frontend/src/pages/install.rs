d656ac33a0d399fbf3ba52c9b5cc83fae7506aef
Elia Bieri <contact@eliabieri.com>
2025-04-08 21:06:28 +0200

    Improve widget installation handling

--- frontend/src/pages/install.rs
+++ frontend/src/pages/install.rs
@@ -10,11 +10,37 @@ use yew::prelude::*;
 use yew_feather::{Download, Loader};
 use yew_router::prelude::*;
 
+async fn load_store_items(
+    widget_store_items: UseStateHandle<Vec<WidgetStoreItem>>,
+    error: UseStateHandle<Option<String>>,
+) {
+    let response = Request::get("/get_store_items").send().await;
+    if let Err(e) = response {
+        error.set(Some(format!("Failed to load widget store items: {}", e)));
+        return;
+    }
+
+    let items = response.unwrap().json::<Vec<WidgetStoreItem>>().await;
+    match items {
+        Ok(items) => {
+            log!(format!("Loaded widget store items: {items:?}"));
+            widget_store_items.set(items);
+        }
+        Err(e) => {
+            error.set(Some(format!(
+                "Failed to deserialize widget store items: {}",
+                e
+            )));
+        }
+    }
+}
+
 async fn install_widget(
     action: InstallAction,
     error: UseStateHandle<Option<String>>,
     is_installing: UseStateHandle<bool>,
     navigator: Navigator,
+    widget_store_items: UseStateHandle<Vec<WidgetStoreItem>>,
 ) {
     is_installing.set(true);
     let response = Request::post("/install_widget")
@@ -32,6 +58,7 @@ async fn install_widget(
         Ok(response) => match response.status() {
             200 => {
                 log!("Successfully installed widget");
+                load_store_items(widget_store_items, error).await;
                 navigator.push(&crate::Route::Home);
             }
             _ => {
@@ -58,25 +85,7 @@ pub fn install() -> Html {
         use_effect_with_deps(
             move |_| {
                 wasm_bindgen_futures::spawn_local(async move {
-                    let response = Request::get("/get_store_items").send().await;
-                    if let Err(e) = response {
-                        error.set(Some(format!("Failed to load widget store items: {}", e)));
-                        return;
-                    }
-
-                    let items = response.unwrap().json::<Vec<WidgetStoreItem>>().await;
-                    match items {
-                        Ok(items) => {
-                            log!(format!("Loaded widget store items: {items:?}"));
-                            widget_store_items.set(items);
-                        }
-                        Err(e) => {
-                            error.set(Some(format!(
-                                "Failed to deserialize widget store items: {}",
-                                e
-                            )));
-                        }
-                    }
+                    load_store_items(widget_store_items, error).await;
                 });
             },
             // Run only on first render (no dependencies)
@@ -99,14 +108,16 @@ pub fn install() -> Html {
         let error = error.clone();
         let is_installing = is_installing.clone();
         let navigator = navigator.clone();
+        let widget_store_items = widget_store_items.clone();
         Callback::from(move |_| {
             if let Some(action) = &*installation_data {
                 let error = error.clone();
                 let is_installing = is_installing.clone();
                 let navigator = navigator.clone();
+                let widget_store_items = widget_store_items.clone();
                 let action = action.clone();
                 wasm_bindgen_futures::spawn_local(async move {
-                    install_widget(action, error, is_installing, navigator).await;
+                    install_widget(action, error, is_installing, navigator, widget_store_items).await;
                 });
             }
         })
@@ -116,6 +127,7 @@ pub fn install() -> Html {
         let error = error.clone();
         let is_installing = is_installing.clone();
         let navigator = navigator.clone();
+        let widget_store_items = widget_store_items.clone();
         Callback::from(move |event: MouseEvent| {
             let value = event
                 .target()
@@ -126,14 +138,60 @@ pub fn install() -> Html {
                 let error = error.clone();
                 let is_installing = is_installing.clone();
                 let navigator = navigator.clone();
+                let widget_store_items = widget_store_items.clone();
                 wasm_bindgen_futures::spawn_local(async move {
                     install_widget(
                         InstallAction::FromStoreItemName(value),
                         error,
                         is_installing,
                         navigator,
+                        widget_store_items,
                     )
                     .await;
+                });
+            }
+        })
+    };
+
+    let on_deinstall_widget = {
+        let error = error.clone();
+        let is_installing = is_installing.clone();
+        let widget_store_items = widget_store_items.clone();
+        Callback::from(move |event: MouseEvent| {
+            let value = event
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlButtonElement>().ok())
+                .map(|btn| btn.value());
+
+            if let Some(widget_name) = value {
+                let error = error.clone();
+                let is_installing = is_installing.clone();
+                let widget_store_items = widget_store_items.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    is_installing.set(true);
+                    let response = Request::get(&format!("/deinstall_widget/{}", widget_name))
+                        .send()
+                        .await;
+                    is_installing.set(false);
+
+                    match response {
+                        Err(e) => {
+                            error.set(Some(format!("Failed to deinstall widget: {}", e)));
+                            log!("Failed to deinstall widget");
+                        }
+                        Ok(response) => match response.status() {
+                            200 => {
+                                log!("Successfully deinstalled widget");
+                                load_store_items(widget_store_items, error).await;
+                            }
+                            _ => {
+                                let response_text = response.text().await;
+                                let error_text = response_text.unwrap_or("No error message".to_string());
+                                error.set(Some(error_text));
+                                log!("Failed to deinstall widget");
+                            }
+                        },
+                    }
                 });
             }
         })
@@ -182,9 +240,14 @@ pub fn install() -> Html {
                                                         <span class="text-slate-300 text-sm font-semibold"> {&item.name} </span>
                                                         <span class="text-slate-300 text-xs"> {&item.description} </span>
                                                     </div>
-                                                    <button class="pt-2 text-gray-300 text-sm font-semibold" value={item.name.clone()} onclick={on_install_widget.clone()} disabled={*is_installing}>
-                                                        {"Install"}
-                                                    </button>
+                                                    <div class="flex flex-row gap-2">
+                                                        <button class="pt-2 text-gray-300 text-sm font-semibold" value={item.name.clone()} onclick={on_deinstall_widget.clone()} disabled={*is_installing}>
+                                                            {"Deinstall"}
+                                                        </button>
+                                                        <button class="pt-2 text-gray-300 text-sm font-semibold" value={item.name.clone()} onclick={on_install_widget.clone()} disabled={*is_installing}>
+                                                            {"Install"}
+                                                        </button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </ConfigCardComponent>
